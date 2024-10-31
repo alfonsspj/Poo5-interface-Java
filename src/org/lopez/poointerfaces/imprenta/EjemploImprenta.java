8f3282e6d162ef9cbf06e03ffce2b14536b01c74
alfonsspj <alfonsspj@gmail.com>
2024-10-31 16:35:01 -0300

    Creando la clase ejemplo con el método main

--- src/org/lopez/poointerfaces/imprenta/EjemploImprenta.java
+++ src/org/lopez/poointerfaces/imprenta/EjemploImprenta.java
@@ -1,4 +1,23 @@
 package org.lopez.poointerfaces.imprenta;
 
+import org.lopez.poointerfaces.imprenta.modelo.*;
+
 public class EjemploImprenta {
+    public static void main(String[] args) {
+        Curriculo cv = new Curriculo("john Doe", "Ingeniero de Sistemas", "Resumen laboral...");
+        cv.addExperiencia("Java");
+        cv.addExperiencia("Oracle DBA");
+        cv.addExperiencia("Spring Framework");
+        cv.addExperiencia("Desarrollador fullstack");
+        cv.addExperiencia("Angular");
+
+        Informe informe = new Informe("Martin Fowler", "James", "Estudio sobre microservicios");
+        imprimir(cv);
+        imprimir(informe);
+
+    }
+
+    public static void imprimir(Hoja imprimible) {
+        System.out.println(imprimible.imprimir());
+    }
 }
